a5a629a381aee178a0c831791ef6213a1405d6e8
kokitree <61878718+kokitree@users.noreply.github.com>
2020-03-11 22:41:00 +0700

    littlemore

--- miyazaki/header.php
+++ miyazaki/header.php
@@ -7,7 +7,7 @@
 		<meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ); ?>" charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 
 		<?php wp_head(); ?>
 
