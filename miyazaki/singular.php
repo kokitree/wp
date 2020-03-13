e061f8535d7fe9ebebcf8e1bc394fc80621148f3
kokitree <61878718+kokitree@users.noreply.github.com>
2020-03-13 10:40:16 +0700

    more

--- miyazaki/singular.php
+++ miyazaki/singular.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main id="site-content">
-
+<center><small>is an Amazon Affiliate. When you buy through links on our site, we may earn an affiliate commission.</small></center>
 	<?php
 
 	if ( have_posts() ) :
